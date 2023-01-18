3789394e462b9a0183dc3033c1cef70d0bceda91
Ranto <ranto.rakotondrajoa@gmail.com>
2023-01-18 21:19:42 +0100

    polished structure

--- additions.h
+++
@@ -1,8 +0,0 @@
-#ifndef ADDITIONS_H
-#define ADDITIONS_H
-
-/*
- * @file additions.h: header file for the module additions 
- * contains the functinos that implement some of the shell programs that are not builtins 
-
-#endif 
